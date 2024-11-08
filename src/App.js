8a72cbb02e375e9d722b03cd6498ef1ab8b4297a
Senpai <pgairola619@gmail.com>
2024-11-08 17:25:50 +0530

    Router added

--- src/App.js
+++ src/App.js
@@ -1,47 +1,15 @@
 import React, { useState } from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import {loginPage} from './Pages/loginPage'
 
-function LoginPage() {
-  const [idNumber, setIdNumber] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Perform login logic here, such as sending a request to an API
-    console.log('ID Number:', idNumber);
-    console.log('Password:', password);
-  };
-
-  return (
-    <div className="login-container" style={{flex:1,
-    flexDirection:'row',
-    alignItems:'center',
-    justifyContent:'center'}}>
-      <h1>Student Login</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="idNumber"
-              style={{lineSpacing:'208'}}>Id Number: </label><br></br>
-          <input
-            type="text"
-            id="idNumber"
-            value={idNumber}
-            onChange={(e) => setIdNumber(e.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <br></br><label htmlFor="password"style={{border:10}}>Password: </label><br></br>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-          <br></br>
-        <button type="submit" style={{flex:1, padding: '10px 15px', backgroundColor: '#007bff', color: 'white', border: 'none', borderRadius: '5px'}}>Sign In</button>
-      </form>
-    </div>
-  );
+function App(){
+  return(
+    <Router>
+      <Routes>
+        <Route path='/' element={<loginPage/>}/>
+      </Routes>
+    </Router>
+  )
 }
 
-export default LoginPage;
+export default App
