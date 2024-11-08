a70f008a2ff615e9bf512d1fb369e8db9e8fb232
Senpai <pgairola619@gmail.com>
2024-11-08 17:41:42 +0530

    ff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
+import React, { Profiler, useState } from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './Pages/loginPage';
+import Dashboard from './Pages/Dashboard';
+import Profile from './Pages/Profile';
 
 function App(){
   return(
     <Router>
       <Routes>
-        <Route path='' element={<LoginPage/>}/>
+        <Route path='/' element={<LoginPage/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/profileinfo' element={<Profile/>}/>
       </Routes>
     </Router>
   )
